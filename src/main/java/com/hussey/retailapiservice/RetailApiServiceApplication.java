b64c0fc37218ea86c7ca97c328dc01245395d617
Dominick <dominick.dechristofaro@gmail.com>
2019-10-22 11:56:09 -0400

    initial push for retail-api-service

---
+++ src/main/java/com/hussey/retailapiservice/RetailApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hussey.retailapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hussey/retailapiservice/RetailApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussey.retailapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
